9cfac8b81a2cf694896b1350293622ab33517ddf
josueggss73 <josueggss73@gmail.com>
2020-08-12 23:40:28 -0600

    all data loaded

--- queryFinal.sql
+++ queryFinal.sql
@@ -6,6 +6,12 @@ SELECT * FROM Sexos
 
 SELECT * FROM GruposEdad
 
-select * from Tumores
+select * from Tumores order by nombreTumor
 
 select * from Poblacion_X_Edad_Sexo_Ano
+
+select * from Tumores_X_GruposEdad
+
+select * from Tumores_X_Cantones
+
+--insert GruposEdad values ('Desconocido')
